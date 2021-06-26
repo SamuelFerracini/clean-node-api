d48f73ce953b75f9e57ad46b864a5c3fab990e01
Samuel Ferracini <samferracini@gmail.com>
2021-06-26 15:47:44 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,12 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    return { statusCode: 400, body: new Error('Missing param: name') }
+    const { body } = httpRequest
+    const { name, email } = body
+
+    if (!name)
+      return { statusCode: 400, body: new Error('Missing param: name') }
+
+    if (!email)
+      return { statusCode: 400, body: new Error('Missing param: email') }
   }
 }
